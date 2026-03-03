1afac0b55094717aeea2b91485b0eb29503a918f
Abrrar <madborakash48@gmail.com>
2026-03-03 08:49:03 +0600

    this commit for the new changes

--- src/components/sections/TechStack.jsx
+++ src/components/sections/TechStack.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import TitleHeader from './TitleHeader'
+import { techStackIcons } from '../../constants'
+import TechIcon from '../Moldels/TechIcon'
+import { useGSAP } from '@gsap/react'
 
 function TechStack() {
+
+  useGSAP(() => {
+    gsap.fromTo(".tech-card", {})
+  })
+
+
   return (
-    <div>TechStack</div>
+    <div id="skills" className="flex-center section-padding">
+      <div className="w-full h-full md:px-10 px-5">
+        <TitleHeader
+          title="My Preferred Tech Stack"
+          sub="The Skills I Bring To the Table"
+        />
+
+        <div className="tech-grid">
+          {techStackIcons.map((icon) => (
+            <div key={icon.id} className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg">
+              <div className="tech-card-animated-bg" />
+              <div className='tech-card-content'>
+                <div className='tech-icon-wrapper'>
+                  <TechIcon model={icon} />
+                </div>
+
+                <div className="padding-x w-full">
+                  <p>{icon.name}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
